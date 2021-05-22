22fb397a8b334d62b8e36c93b27cab3830730045
DevHyperCoder <arduinoleo88@gmail.com>
2021-05-22 19:16:13 +0530

    win roles

--- config.def.h
+++ config.def.h
@@ -36,7 +36,7 @@ static const char *tags[] = {
     "",
     "磊",
     "ﭮ",
-    "",
+    "",
     "" };
 
 static const Rule rules[] = {
@@ -46,8 +46,10 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "firefox", NULL,     NULL,           1 << 7,    0,          0,          -1,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "discord", NULL,     NULL,           1 << 6,    0,          0,           0,        -1 },
+    { "Brave-browser",NULL,".*youTube.*",  1<<5,        0       ,0,0,-1},
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
